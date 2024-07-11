3f05390322239c7c6eca2755155a6d06156366d0
താനോസ് <145189212+sivaoptr@users.noreply.github.com>
2024-07-11 13:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors=require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.get("/",(req,res)=>{res.send("This is Counsellor web personal use only..")})
+app.get("/",(req,res)=>{res.send("How can i Help you")})
 app.post("/contactUs",async(req,res)=>{
      let result=await mailing(req.body.name,req.body.email,req.body.feedback)
      if(result){res.json({ok:true,text:"sended"})}
