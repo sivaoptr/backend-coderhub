7cd8387987ba500ae50ba7a437e8ef49e88afe48
താനോസ് <145189212+sivaoptr@users.noreply.github.com>
2024-07-11 13:59:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-const express = require('express');
-const app = express();
-const mailing = require('./mail/mail.js');
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.send("How can I help you?");
-});
-
-app.post("/contactUs", async (req, res) => {
-    try {
-        let result = await mailing(req.body.name, req.body.email, req.body.feedback);
-        if (result) {
-            res.json({ ok: true, text: "Message sent" });
-        } else {
-            res.json({ ok: false, text: "Error in sending message" });
-        }
-    } catch (error) {
-        console.error("Error in /contactUs endpoint:", error);
-        res.status(500).json({ ok: false, text: "Server error" });
-    }
-});
-
-app.listen(5000, () => console.log("App listening on port 5000"));
+const express=require('express')
+const app=express()
+const mailing=require('./mail/mail.js')
+const cors=require('cors')
+const corsOptions = {
+  origin: 'https://codeharborhub.github.io', // Allow requests from this origin
+  methods: ['GET', 'POST'], // Allow only GET and POST requests
+    allowedHeaders: ['Content-Type'], // Allow only headers with Content-Type
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.get("/",(req,res)=>{res.send("This is Counsellor web personal use only..")})
+app.post("/contactUs",async(req,res)=>{
+     let result=await mailing(req.body.name,req.body.email,req.body.feedback)
+     if(result){res.json({ok:true,text:"sended"})}
+     else{res.json({ok:false,text:"Error"})}  
+})
+app.listen(5000,()=>console.log("app listen in 5000"))
